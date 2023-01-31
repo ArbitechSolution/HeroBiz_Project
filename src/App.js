f74a43186a28053f59f72b3978ac20f05d1ac218
Noman Ahmad <nomanahmad12100@gmail.com>
2023-01-31 11:38:20 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'
+import Navbar from './Components/Navbar/Navbar';
+import Cards from './Components/Cards/Cards';
+import Footer from './Components/Footer/Footer';
+
+
+function App() {
+  return (
+   <>
+<Navbar />
+<Cards />
+<Footer />
+   </>
+  );
+}
+
+export default App;
